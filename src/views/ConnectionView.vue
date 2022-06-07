90e91408d437109c1f4cbaa42531ff92ccb88ed4
Thierry Charriot <mrthierrycharriot@gmail.com>
2022-06-07 14:09:32 +0200

    WIP components inscription and connection

--- src/views/ConnectionView.vue
+++ src/views/ConnectionView.vue
@@ -1,10 +1,23 @@
 <template>
 
-    <HeaderComponent />
-    <div class="container">
-      <h1 class="alert alert-success" role="alert">Connection!</h1>
-    </div>
-    <!--<div></div>-->
+  <HeaderComponent />
+  <div class="container">
+    <h1 class="alert alert-success" role="alert">Connection!</h1>
+    <p v-for="error in errors" :key="error">{{ error }}</p>
+
+    <fieldset class="mb-3">
+      <div class="mb-3">
+        <label for="username" class="form-label">Username</label>
+        <input type="text" class="form-control" id="username" aria-describedby="emailHelp" v-model="username">
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" v-model="password">
+      </div>
+      <button class="btn btn-success" v-on:click="sendForm">Connection account</button>
+    </fieldset>
+  </div>
+  <!--<div></div>-->
 
 </template>
 
@@ -12,24 +25,43 @@
 // @ is an alias to /src
 import HeaderComponent from '@/components/HeaderComponent.vue'
 
-  export default {
-    name: 'ConnectionView',
-    components: {
-      HeaderComponent
-    },
-    props: {},
-    data() {
-      return {}
-    },
-    computed: {},
-    mounted() {},
-    methods: {}
+export default {
+  name: 'ConnectionView',
+  components: {
+    HeaderComponent
+  },
+  props: {},
+  data() {
+    return {
+      errors: [],
+      username: null,
+      password: null
+    }
+  },
+  computed: {},
+  mounted() { },
+  methods: {
+    sendForm() {
+      this.errors = [];
+      if (!this.username) {
+        this.errors.push("Username must not be empty");
+      }
+      if (!this.role) {
+        this.errors.push("Role must not be empty");
+      }
+      if (!this.password) {
+        this.errors.push("Password must not be empty");
+      } 
+      console.log('IT WORKS!');
+      // @TODO Executer une requete Asynchrone pour inscrire un nouvel utilisateur
+    }
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	.alert {
-		margin-top: 3rem;
-	}
+.alert {
+  margin-top: 3rem;
+}
 </style>
--- src/views/InscriptionView.vue
+++ src/views/InscriptionView.vue
@@ -1,10 +1,41 @@
 <template>
 
-    <HeaderComponent />
-    <div class="container">
-      <h1 class="alert alert-success" role="alert">Inscription!</h1>
-    </div>
-    <!--<div></div>-->
+  <HeaderComponent />
+
+  <div class="container">
+
+    <h1 class="alert alert-success" role="alert">Inscription!</h1>
+    <p v-for="error in errors" :key="error">{{ error }}</p>
+
+    <fieldset class="mb-3">
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" v-model="email">
+      </div>
+      <div class="mb-3">
+        <label for="username" class="form-label">Username</label>
+        <input type="text" class="form-control" id="username" aria-describedby="emailHelp" v-model="username">
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" v-model="password">
+      </div>
+      <div class="mb-3">
+        <label for="password_check" class="form-label">Password check</label>
+        <input type="password" class="form-control" id="password_check" v-model="password_check">
+      </div>
+      <div class="mb-3">
+        <label for="form-select" class="form-label">Choose role</label>
+        <select class="form-select" aria-label="Default select example" v-model="role">
+          <option value="cook">Cook</option>
+          <option value="subscriber">Subscriber</option>
+        </select>
+      </div>
+      <button class="btn btn-success" v-on:click="sendForm">Create account</button>
+    </fieldset>
+
+  </div>
+  <!--<div></div>-->
 
 </template>
 
@@ -12,24 +43,53 @@
 // @ is an alias to /src
 import HeaderComponent from '@/components/HeaderComponent.vue'
 
-  export default {
-    name: 'InscriptionView',
-    components: {
-      HeaderComponent
-    },
-    props: {},
-    data() {
-      return {}
-    },
-    computed: {},
-    mounted() {},
-    methods: {}
+export default {
+  name: 'InscriptionView',
+  components: {
+    HeaderComponent
+  },
+  props: {},
+  data() {
+    return {
+      errors: [],
+      email: null,
+      username: null,
+      password: null,
+      password_check: null,
+      role: null
+    }
+  },
+  computed: {},
+  mounted() { },
+  methods: {
+    sendForm() {
+      this.errors = [];
+      if (!this.email) {
+        this.errors.push("Email must not be empty");
+      }
+      if (!this.username) {
+        this.errors.push("Username must not be empty");
+      }
+      if (!this.role) {
+        this.errors.push("Role must not be empty");
+      }
+      if (!this.password || !this.password_check) {
+        this.errors.push("Password and Password_check must be filled");
+      } else {
+        if (this.password !== this.password_check) {
+          this.errors.push("Password and Password_check must be the same");
+        }
+      }
+      console.log('IT WORKS!');
+      // @TODO Executer une requete Asynchrone pour inscrire un nouvel utilisateur
+    }
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	.alert {
-		margin-top: 3rem;
-	}
+.alert {
+  margin-top: 3rem;
+}
 </style>
